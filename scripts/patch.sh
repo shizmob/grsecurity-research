fcc6c53ea2f218e500990b61c73dc8e051fae014
Shiz <hi@shiz.me>
2017-05-01 06:32:23 +0200

    Some more hunks, fix up patch.sh.

--- scripts/patch.sh
+++ scripts/patch.sh
@@ -6,20 +6,21 @@ if ! test -d linux-$kernver ; then
 	curl -L https://cdn.kernel.org/pub/linux/kernel/v${kernver%%.*}.x/linux-$kernver.tar.xz | tar -xJ
 fi
 
-if test -f linux-$kernver/.applied-remainder.patch ; then
-	patch -ts -R -d linux-$kernver < linux-$kernver/.applied-remainder.patch
-	rm linux-$kernver/.applied-remainder.patch
-fi
+# Undo applied patches.
+find linux-$kernver -name '.applied-*.patch' | sort -r | while read patch ; do
+	case "$patch" in */.applied-xx-*) args=-s ;; *) args= ;; esac
+	echo ">> reverting $(basename "$patch")..."
+	patch $args -t -R -d linux-$kernver -p1 < "$patch" || exit 1
+	rm "$patch"
+done
 
+# Apply patches again.
 find split -name '*.patch' | while read f; do printf "%s:%s\n" "$(basename "$f")" "$f" ; done | sort | cut -d: -f2- | while read patch ; do
 	base="$(basename "$patch")"
 	test -f linux-$kernver/.applied-"$base" && continue
-	echo ">> applying ${patch#split/}..."
-	patch -ts -d linux-$kernver -p1 < "$patch" && touch linux-$kernver/.applied-"$base" || exit 1
-done
+	case "$base" in xx-*) args=-s ;; *) args= ;; esac
 
-if test -z "$SLIM" ; then
-	echo ">> applying remainder..."
-	patch -ts -d linux-$kernver -p1 < "grsecurity-$grsecver-$kernver-$grsecdate.patch"
-	cp "grsecurity-$grsecver-$kernver-$grsecdate.patch" linux-$kernver/.applied-remainder.patch
-fi
+	echo ">> applying ${patch#split/}..."
+	patch $args -t -N -d linux-$kernver -p1 < "$patch" || exit 1
+	cp "$patch" linux-$kernver/.applied-"$base"
+done
--- scripts/progress.sh
+++ scripts/progress.sh
@@ -3,10 +3,10 @@ set -e
 . scripts/common.sh
 
 printf "split hunks: "
-find split -type f -name '*.patch' -exec grep -ch '^@@' {} + | awk '{ n += $1 } END { print n }'
+find split -type f -name '*.patch' -and -not -name 'xx-*.patch' -exec grep -ch '^@@' {} + | awk '{ n += $1 } END { print n }'
 
 printf "remaining hunks: "
-grep -crh '^@@' grsecurity-$grsecver-$kernver-$grsecdate.patch
+grep -crh '^@@' split/xx-grsecurity-$grsecver-$kernver-$grsecdate.patch
 
 printf "original hunks: "
-grep -crh '^@@' orig-grsecurity-$grsecver-$kernver-$grsecdate.patch
+grep -crh '^@@' grsecurity-$grsecver-$kernver-$grsecdate.patch
